6fed46d0f6cf67672887f6b8e5a79404f900b5d4
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-06 16:30:51 +0100

    Actualización del procesamiento del token en el frontend y estilos leves

--- src-frontend/src/app/componentes/vistas/vista-login/vista-login.component.ts
+++ src-frontend/src/app/componentes/vistas/vista-login/vista-login.component.ts
@@ -1,30 +1,55 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AuthenticationService } from '../../../servicios/authentication/authentication.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { LoginInterface } from '../../../interfaces/login-interface';
+import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vista-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './vista-login.component.html',
   styleUrl: './vista-login.component.scss',
 })
-export class VistaLoginComponent {
-  email: string = '';
-  contrasena: string = '';
+export class VistaLoginComponent implements OnInit {
+  credenciales: LoginInterface = { email: '', contrasena: '' };
+  usuarioLogueado: boolean = false;
 
-  constructor(
-    private authService: AuthenticationService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthenticationService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.usuarioLogueado = this.authService.comprobarLogueado();
+  }
 
   login() {
-    this.authService
-      .login(this.email, this.contrasena)
-      .subscribe((response) => {
+    this.authService.login(this.credenciales).subscribe({
+      next: (response) => {
         localStorage.setItem('token', response.token);
-        this.router.navigate(['/ejemplo']);
-      });
+        this.usuarioLogueado = true;
+
+        const nombreUsuario = this.extraerNombreUsuario(this.credenciales.email);
+        Swal.fire({
+          title: 'Inicio de sesión exitoso',
+          text: `¡Bienvenido, ${nombreUsuario}!`,
+          icon: 'success',
+        });
+
+        this.router.navigate(['/vistaprincipal']);
+      },
+      error: (error) => {
+        Swal.fire({
+          title: 'Error en la petición',
+          text: 'No hemos podido conectar con el servidor',
+          icon: 'error',
+        });
+      },
+      complete: () => {},
+    });
+  }
+
+  private extraerNombreUsuario(email: string): string {
+    return email.substring(0, email.indexOf('@'));
   }
 }
