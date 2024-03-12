5ba7caa417a5771e874e3288ad76608fd8ffd427
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-12 15:45:09 +0100

    Creado método "actualizarEstadoLogueado" para cambiar estado de login y emitir evento con el estado y controlarlo en los distintos componentes

--- src-frontend/src/app/servicios/authentication/authentication.service.ts
+++ src-frontend/src/app/servicios/authentication/authentication.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoginInterface } from '../../interfaces/login-interface';
@@ -9,6 +9,7 @@ import { LoginInterface } from '../../interfaces/login-interface';
 export class AuthenticationService {
   private urlBase = 'http://localhost:8080/api/auth';
   private usuarioLogueado: boolean = false;
+  usuarioLogueadoCambio: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(private http: HttpClient) {}
 
@@ -20,5 +21,10 @@ export class AuthenticationService {
     return this.http.post<any>(`${this.urlBase}/signin`, credenciales, {
       withCredentials: true,
     });
+  }
+
+  actualizarEstadoLogueado(estado: boolean) {
+    this.usuarioLogueado = estado;
+    this.usuarioLogueadoCambio.emit(estado);
   }
 }
