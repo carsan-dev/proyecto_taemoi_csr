921c3002e98e3aba13904f666edf53d8aa8fd274
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-12 15:37:19 +0100

    Correción del routing a la página principal

--- src-frontend/src/app/app.routes.ts
+++ src-frontend/src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { VistaLoginComponent } from './componentes/vistas/vista-login/vista-login.component';
 import { EscaparatePrincipalComponent } from './componentes/vistas/escaparate-principal/escaparate-principal.component';
 import { VistaPrincipalAdminComponent } from './componentes/vistas/vista-principal-admin/vista-principal-admin.component';
-import { SkeletonComponent } from './componentes/vistas/layout/skeleton/skeleton.component';
 import { ListadoAlumnosCompletoDTOComponent } from './componentes/endpoints/listado-alumnos-completo/listado-alumnos-completo.component';
 
 export const routes: Routes = [
-  { path: '', component: SkeletonComponent, outlet: 'skeleton' },
   { path: '', component: EscaparatePrincipalComponent },
   { path: 'login', component: VistaLoginComponent },
-  { path: 'vistaprincipal', component: VistaPrincipalAdminComponent },
+  { path: 'vista-principal', component: VistaPrincipalAdminComponent },
   { path: 'listar-alumnos', component: ListadoAlumnosCompletoDTOComponent}
 ];
 
