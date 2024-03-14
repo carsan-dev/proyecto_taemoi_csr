f83df91bfdb8591953847c2a414b950a0cb440a3
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-14 13:59:18 +0100

    Añadida nueva ruta para el componente crear alumno

--- src-frontend/src/app/app.routes.ts
+++ src-frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { VistaLoginComponent } from './componentes/vistas/vista-login/vista-logi
 import { EscaparatePrincipalComponent } from './componentes/vistas/escaparate-principal/escaparate-principal.component';
 import { VistaPrincipalAdminComponent } from './componentes/vistas/vista-principal-admin/vista-principal-admin.component';
 import { ListadoAlumnosCompletoDTOComponent } from './componentes/endpoints/listado-alumnos-completo/listado-alumnos-completo.component';
+import { CrearAlumnoComponent } from './componentes/endpoints/crear-alumno/crear-alumno.component';
 
 export const routes: Routes = [
   { path: '', component: EscaparatePrincipalComponent },
   { path: 'login', component: VistaLoginComponent },
   { path: 'adminpage', component: VistaPrincipalAdminComponent },
-  { path: 'alumnos', component: ListadoAlumnosCompletoDTOComponent}
+  { path: 'alumnos', component: ListadoAlumnosCompletoDTOComponent},
+  { path: 'alumnos/crear', component: CrearAlumnoComponent}
 ];
 
 @NgModule({
