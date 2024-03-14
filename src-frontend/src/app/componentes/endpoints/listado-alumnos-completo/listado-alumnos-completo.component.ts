656b7b17788359ba492188af84eac5728787a26d
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-14 14:03:06 +0100

    Corrección del componente paginación e implementación correcta del mismo

--- src-frontend/src/app/componentes/endpoints/listado-alumnos-completo/listado-alumnos-completo.component.ts
+++ src-frontend/src/app/componentes/endpoints/listado-alumnos-completo/listado-alumnos-completo.component.ts
@@ -3,26 +3,30 @@ import { EndpointsService } from '../../../servicios/endpoints/endpoints.service
 import { CommonModule } from '@angular/common';
 import { SidebarService } from '../../../servicios/generales/sidebar.service';
 import { SidebarComponent } from '../../vistas/layout/sidebar/sidebar.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listado-alumnos-completo',
   standalone: true,
   imports: [CommonModule, SidebarComponent],
   templateUrl: './listado-alumnos-completo.component.html',
-  styleUrl: './listado-alumnos-completo.component.scss'
+  styleUrl: './listado-alumnos-completo.component.scss',
 })
 export class ListadoAlumnosCompletoDTOComponent {
   alumnos: any[] = [];
-  currentPage: number = 1;
-  pageSize: number = 1;
-  totalPages: number = 0;
-  totalPagesArray: number[] = [];
+  paginaActual: number = 1;
+  tamanoPagina: number = 1;
+  totalPaginas: number = 0;
+  displayPages: number[] = [];
 
-  constructor(private endpointsService: EndpointsService, private sidebarService: SidebarService) {}
+  constructor(
+    private endpointsService: EndpointsService,
+    private sidebarService: SidebarService
+  ) {}
 
   ngOnInit(): void {
     if (typeof localStorage !== 'undefined') {
-    this.obtenerAlumnos();
+      this.obtenerAlumnos();
     }
   }
 
@@ -30,26 +34,48 @@ export class ListadoAlumnosCompletoDTOComponent {
     const token = localStorage.getItem('token');
 
     if (token) {
-      this.endpointsService.enviarToken(token, this.currentPage, this.pageSize).subscribe({
-        next: (response) => {
-          this.alumnos = response.content;
-          this.totalPages = response.totalPages;
-          this.calcularTotalPagesArray();
-        },
-        error: (error) => {
-          console.log(error);
-        },
-      });
+      this.endpointsService
+        .enviarToken(token, this.paginaActual, this.tamanoPagina)
+        .subscribe({
+          next: (response) => {
+            this.alumnos = response.content;
+            this.totalPaginas = response.totalPages;
+            this.actualizarDisplayPages();
+          },
+          error: (error) => {
+            Swal.fire({
+              title: 'Error en la petición',
+              text: 'No hemos podido conectar con el servidor',
+              icon: 'error',
+            });
+          },
+        });
     }
   }
 
   cambiarPagina(pageNumber: number): void {
-    this.currentPage = pageNumber;
+    this.paginaActual = pageNumber;
     this.obtenerAlumnos();
   }
 
-  calcularTotalPagesArray() {
-    this.totalPagesArray = Array(this.totalPages).fill(0).map((x, i) => i + 1);
+  actualizarDisplayPages() {
+    const paginasAMostrar = 5;
+    const mitadDePaginasAMostrar = Math.floor(paginasAMostrar / 2);
+    let paginaInicio = this.paginaActual - mitadDePaginasAMostrar;
+    let paginaFin = this.paginaActual + mitadDePaginasAMostrar;
+
+    if (paginaInicio < 1) {
+      paginaInicio = 1;
+      paginaFin = Math.min(this.totalPaginas, paginasAMostrar);
+    } else if (paginaFin > this.totalPaginas) {
+      paginaFin = this.totalPaginas;
+      paginaInicio = Math.max(1, this.totalPaginas - paginasAMostrar + 1);
+    }
+
+    this.displayPages = Array.from(
+      { length: paginaFin - paginaInicio + 1 },
+      (_, i) => paginaInicio + i
+    );
   }
 
   alternarVisibilidadSidebar(): void {
