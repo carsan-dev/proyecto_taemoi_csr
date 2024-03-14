dc05134495eace1cd199f77519e7c1a4ea18f6dd
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-14 01:11:46 +0100

    Creación, estilización y programación de sidebar que se esconde con un botón

--- src-frontend/src/app/componentes/vistas/layout/sidebar/sidebar.component.ts
+++ src-frontend/src/app/componentes/vistas/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SidebarService } from '../../../../servicios/generales/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  estaColapsado: boolean = false;
 
+  constructor(private router: Router, private sidebarService: SidebarService) {}
+
+  ngOnInit(): void {
+    this.sidebarService.obtenerSubjectAlternable().subscribe(() => {
+      this.alternarColapso();
+    });
+  }
+  alternarColapso() {
+    this.estaColapsado = !this.estaColapsado;
+  }
+
+  irAListado() {
+    this.router.navigate(['/alumnos']);
+  }
 }
