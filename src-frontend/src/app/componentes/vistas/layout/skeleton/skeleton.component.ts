0e82eb814104d32e547db01c33a9f2e00bf8706f
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-15 18:31:38 +0100

    Integración del botón de scroll al componente skeleton

--- src-frontend/src/app/componentes/vistas/layout/skeleton/skeleton.component.ts
+++ src-frontend/src/app/componentes/vistas/layout/skeleton/skeleton.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { BotonscrollComponent } from '../../../generales/botonscroll/botonscroll.component';
 
 @Component({
   selector: 'app-skeleton',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BotonscrollComponent],
   templateUrl: './skeleton.component.html',
   styleUrl: './skeleton.component.scss'
 })
