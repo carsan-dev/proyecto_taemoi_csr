8a994943d2780320f1706051ffb5db3b4508a374
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-16 21:08:39 +0100

    Eliminación de redirección innecesaria en vista principal admin

--- src-frontend/src/app/componentes/vistas/vista-principal-admin/vista-principal-admin.component.ts
+++ src-frontend/src/app/componentes/vistas/vista-principal-admin/vista-principal-admin.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
 
 @Component({
@@ -14,16 +13,10 @@ export class VistaPrincipalAdminComponent implements OnInit {
   token: string | null = null;
   tokenExiste: boolean = false;
 
-  constructor(private router: Router) {}
-
   ngOnInit(): void {
     if (typeof localStorage !== 'undefined') {
       this.token = localStorage.getItem('token');
       this.tokenExiste = !!this.token;
     }
-  }
-
-  irAListado() {
-    this.router.navigate(['/alumnos']);
   }
 }
