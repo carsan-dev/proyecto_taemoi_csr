83e571fdb50c8f7783c9345343bd33a43dee8fb9
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-16 18:14:26 +0100

    Creación del enum tipogrado

---
+++ src-frontend/src/app/enums/tipo-grado.ts
@@ -0,0 +1,15 @@
+export enum TipoGrado {
+  BLANCO = 'Blanco',
+  BLANCO_AMARILLO = 'Blanco-Amarillo',
+  AMARILLO = 'Amarillo',
+  AMARILLO_NARANJA = 'Amarillo-Naranja',
+  NARANJA = 'Naranja',
+  NARANJA_VERDE = 'Naranja-Verde',
+  VERDE = 'Verde',
+  VERDE_AZUL = 'Verde-Azul',
+  AZUL = 'Azul',
+  AZUL_ROJO = 'Azul-Rojo',
+  ROJO = 'Rojo',
+  ROJO_NEGRO = 'Rojo-Negro',
+  NEGRO = 'Negro',
+}
