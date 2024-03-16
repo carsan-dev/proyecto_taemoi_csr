541c4deac5afebff8a874745a85c057b5695e2ff
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-16 22:20:35 +0100

    Refactorización de la obtención de alumnos e implementación del endpoint de actualizar los mismos

--- src-frontend/src/app/servicios/endpoints/endpoints.service.ts
+++ src-frontend/src/app/servicios/endpoints/endpoints.service.ts
@@ -3,50 +3,55 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EndpointsService {
   private urlBase = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) {}
 
-  obtenerAlumnosDTO(token: string, page: number, size: number): Observable<any> {
+  obtenerAlumnos(token: string, page: number, size: number): Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
 
     const params = new HttpParams()
       .set('page', page.toString())
       .set('size', size.toString());
 
-    return this.http.get<any>(`${this.urlBase}/alumnos`, { headers: headers, params: params });
-  }
-
-  obtenerAlumnos(token: string, page: number, size: number): Observable<any> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
+    return this.http.get<any>(`${this.urlBase}/alumnos`, {
+      headers: headers,
+      params: params,
     });
-
-    const params = new HttpParams()
-    .set('page', page.toString())
-    .set('size', size.toString());
-
-    return this.http.get<any>(`${this.urlBase}/admin`, { headers: headers, params: params });
   }
 
   crearAlumno(nuevoAlumno: any, token: string) {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
 
-    return this.http.post<any>(`${this.urlBase}/alumnos`, nuevoAlumno, { headers: headers });
+    return this.http.post<any>(`${this.urlBase}/alumnos`, nuevoAlumno, {
+      headers: headers,
+    });
+  }
+
+  actualizarAlumno(id: number, alumnoActualizado: any, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.put<any>(`${this.urlBase}/alumnos/${id}`, alumnoActualizado, {
+      headers: headers,
+    });
   }
 
   eliminarAlumnos(id: number, token: string): Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
 
-    return this.http.delete<any>(`${this.urlBase}/alumnos/${id}`, { headers: headers });
+    return this.http.delete<any>(`${this.urlBase}/alumnos/${id}`, {
+      headers: headers,
+    });
   }
 }
