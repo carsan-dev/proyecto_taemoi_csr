879fc17c104611084c3595fb67e5366dd44dd567
csanrom1702 <csanrom1702@g.educaand.es>
2024-03-29 13:30:54 +0100

    Refactorización del uso de imágenes con la interfaz de imágenes

--- src-frontend/src/app/componentes/vistas/escaparate-principal/escaparate-principal.component.ts
+++ src-frontend/src/app/componentes/vistas/escaparate-principal/escaparate-principal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../../servicios/authentication/authentication.service';
 import { CommonModule } from '@angular/common';
+import { ImagenInterface } from '../../../interfaces/imagen-interface';
 
 @Component({
   selector: 'app-escaparate-principal',
@@ -11,28 +12,28 @@ import { CommonModule } from '@angular/common';
 })
 export class EscaparatePrincipalComponent implements OnInit {
   usuarioLogueado: boolean = false;
-  imagenes = [
+  imagenes: ImagenInterface[] = [
     {
-      src: 'assets/media/fachada_escuela.webp',
-      alt: 'Primera imagen del carrousel de la fachada de la escuela de taekwondo con un cartel que representa el nombre de la escuela y otro en el que se muestran los valores que se instruyen.',
+      imgSrc: 'assets/media/fachada_escuela.webp',
+      imgAlt: 'Primera imagen del carrousel de la fachada de la escuela de taekwondo con un cartel que representa el nombre de la escuela y otro en el que se muestran los valores que se instruyen.',
       captionTitulo: "¡Bienvenid@ a MOI'S KIM DO!",
       captionTexto: 'Descubre un lugar donde los valores de respeto, disciplina y superación se convierten en tu mejor aliado.',
     },
     {
-      src: 'assets/media/recepcion_escuela.webp',
-      alt: 'Segunda imagen del carrousel de la recepcion de la escuela de taekwondo en la que se muestra la mesa de la entrada donde se solicitan todo tipo de gestiones.',
+      imgSrc: 'assets/media/recepcion_escuela.webp',
+      imgAlt: 'Segunda imagen del carrousel de la recepcion de la escuela de taekwondo en la que se muestra la mesa de la entrada donde se solicitan todo tipo de gestiones.',
       captionTitulo: '¡Tu paso hacia la grandeza!',
       captionTexto: 'Inicia tu camino hacia la maestría adentrándote en nuestro espacio de bienvenida y asesoramiento.',
     },
     {
-      src: 'assets/media/interior_escuela.webp',
-      alt: 'Tercera imagen del carrousel con el tatami de la escuela de taekwondo en la que se muestra cómo es la zona de entrenamiento de la misma.',
+      imgSrc: 'assets/media/interior_escuela.webp',
+      imgAlt: 'Tercera imagen del carrousel con el tatami de la escuela de taekwondo en la que se muestra cómo es la zona de entrenamiento de la misma.',
       captionTitulo: 'Domina tu destino',
       captionTexto: 'Adéntrate en un entorno de entrenamiento espacioso y agradable diseñado para elevar tu habilidad y alcanzar tus metas.',
     },
     {
-      src: 'assets/media/maquinas_escuela.webp',
-      alt: 'Cuarta imagen del carrousel con la zona de la escuela de taekwondo donde están las distintas máquinas para hacer ejercicios.',
+      imgSrc: 'assets/media/maquinas_escuela.webp',
+      imgAlt: 'Cuarta imagen del carrousel con la zona de la escuela de taekwondo donde están las distintas máquinas para hacer ejercicios.',
       captionTitulo: 'Y por si fuera poco...',
       captionTexto: 'Experimenta el poder del equipo de calidad diseñado para desafiar tus límites y fortalecer tu cuerpo y mente.',
     },
