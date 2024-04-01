a6c98b2d5f4c569f189e96840dd107f949ceea75
csanrom1702 <csanrom1702@g.educaand.es>
2024-04-01 13:48:35 +0200

    Actualización del servicio para usar la interfaz eventosInterface

--- src-frontend/src/app/servicios/generales/eventos.service.ts
+++ src-frontend/src/app/servicios/generales/eventos.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
+import { EventosInteface } from '../../interfaces/eventos-inteface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventosService {
-  eventos = [
-    { nombre: 'Conferencia Angular', fecha: '2024-04-10', imagenUrl: '', descripcion: ''},
-    { nombre: 'Taller de Desarrollo Web', fecha: '2024-04-15', imagenUrl: '', descripcion: ''},
-    { nombre: 'Reunión de Networking', fecha: '2024-04-20', imagenUrl: '', descripcion: ''}
+  eventos: EventosInteface[] = [
+    { titulo: 'Conferencia Angular', fecha: '2024-04-10', imagenUrl: '', descripcion: ''},
+    { titulo: 'Taller de Desarrollo Web', fecha: '2024-04-15', imagenUrl: '', descripcion: ''},
+    { titulo: 'Reunión de Networking', fecha: '2024-04-20', imagenUrl: '', descripcion: ''}
   ];
 
   constructor() { }
 
-  obtenerEventos() {
+  obtenerEventos(): EventosInteface[] {
     return this.eventos;
   }
 }
