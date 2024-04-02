5f6dee90a3e067a2a2ccb0816279d99219fb70c2
csanrom1702 <csanrom1702@g.educaand.es>
2024-04-02 17:15:33 +0200

    Añadidos eventos reales pendientes de imágenes

--- src-frontend/src/app/servicios/generales/eventos.service.ts
+++ src-frontend/src/app/servicios/generales/eventos.service.ts
@@ -6,7 +6,15 @@ import { EventosInteface } from '../../interfaces/eventos-inteface';
 })
 export class EventosService {
   eventos: EventosInteface[] = [
-    { titulo: 'Campeonato de Andalucía Cadete', fecha: '27 de Abril de 2024', imagenUrl: 'assets/media/campeonato_andalucia_cadete.webp', descripcion: 'Pulsar aquí para más información'},
+    { titulo: 'Campeonato Andalucía Cadete', fecha: '27 de Abril de 2024', imagenUrl: 'assets/media/campeonato_andalucia_cadete_2024-04-27.webp'},
+    { titulo: 'Campeonato Andalucía Junior Pesos Olímpicos', fecha: '04 de Mayo de 2024', imagenUrl: ''},
+    { titulo: 'Campeonato Andalucía Combate Deporte Base', fecha: '08 de Junio de 2024', imagenUrl: ''},
+    { titulo: 'Campeonato de Andalucía Nocturno Campeón de Campeones Precadete y Cadete', fecha: '29 de Junio de 2024', imagenUrl: ''},
+    { titulo: 'Programa Jóvenes Talentos Cadete', fecha: '21 de Septiembre de 2024', imagenUrl: ''},
+    { titulo: 'Programa Jóvenes Talentos Junior y Entrenamiento Abierto', fecha: '22 de Septiembre de 2024', imagenUrl: ''},
+    { titulo: 'Supercopa Andalucía Junior', fecha: '05 de Octubre de 2024', imagenUrl: ''},
+    { titulo: 'Supercopa Andalucía Cadete', fecha: '26 de Octubre de 2024', imagenUrl: ''},
+    { titulo: 'Campeonato Andalucía Junior 2025', fecha: '02 de Noviembre de 2024', imagenUrl: ''},
   ];
 
   constructor() { }
