6422d7bd4457abf2cbed0548734896c50f94d74c
csanrom1702 <csanrom1702@g.educaand.es>
2024-04-10 17:40:46 +0200

    Creadas excepciones para los ficheros

---
+++ src-api/src/main/java/com/taemoi/project/errores/fichero/StorageException.java
@@ -0,0 +1,13 @@
+package com.taemoi.project.errores.fichero;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
