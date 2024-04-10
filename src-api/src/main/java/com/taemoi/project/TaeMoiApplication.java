3ce39956c301014f7f1c3beabff3cef83d967db0
csanrom1702 <csanrom1702@g.educaand.es>
2024-04-10 17:38:28 +0200

    Inicialización y borrado de ficheros al cargar la app

--- src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
+++ src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
@@ -1,12 +1,24 @@
 package com.taemoi.project;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.taemoi.project.servicios.FicheroService;
 
 @SpringBootApplication
 public class TaeMoiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaeMoiApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner init(FicheroService ficheroService) {
+		return args -> {
+			ficheroService.deleteAll();
+			ficheroService.init();
+		};
 	}
 }
