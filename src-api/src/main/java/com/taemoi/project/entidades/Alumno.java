6e38aa0153ed9882c8105f3eeaaab6dbb8ed72a2
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-11 18:21:32 +0200

    Añadido el CascadeType a alumno para eliminar su imagen junto con el alumno

--- src-api/src/main/java/com/taemoi/project/entidades/Alumno.java
+++ src-api/src/main/java/com/taemoi/project/entidades/Alumno.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -68,8 +69,8 @@ public class Alumno {
 	@Temporal(TemporalType.DATE)
 	private Date fechaBaja;
 	
-	@OneToOne
-	private Imagen fotoAlumno;
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Imagen fotoAlumno;
 
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "categoria_id")
