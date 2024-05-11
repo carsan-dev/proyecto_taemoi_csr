8b3b71acb6f998fb1af1baafcb74c76448464289
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-11 14:10:11 +0200

    Eliminada la inicialización de ficheros al arrancar el servidor

--- src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
+++ src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
@@ -5,19 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.taemoi.project.servicios.FicheroService;
-
 @SpringBootApplication
 public class TaeMoiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaeMoiApplication.class, args);
-	}
-	
-	@Bean
-	public CommandLineRunner init(FicheroService ficheroService) {
-		return args -> {
-			ficheroService.init();
-		};
 	}
 }
