d3752bc8ba647428e24b8e491c9852d98f5e9f5d
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-15 16:14:25 +0200

    Creado el servicio de turnos para la modificación, creación, eliminación y listado de los mismos, junto a la lógica asignación a grupos

---
+++ src-api/src/main/java/com/taemoi/project/servicios/TurnoService.java
@@ -0,0 +1,22 @@
+package com.taemoi.project.servicios;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.taemoi.project.dtos.TurnoDTO;
+import com.taemoi.project.entidades.Turno;
+
+public interface TurnoService {
+	
+	List<Turno> listarTurnos();
+	
+	List<TurnoDTO> listarTurnosDTO();
+	
+	void crearTurnoSinGrupo(TurnoDTO turnoDTO);
+	
+	void crearTurnoYAsignarAGrupo(TurnoDTO turnoDTO);
+	
+	TurnoDTO actualizarTurno(Long turnoId, TurnoDTO turnoDTO);
+	
+	void eliminarTurno(Long turnoId);
+}
