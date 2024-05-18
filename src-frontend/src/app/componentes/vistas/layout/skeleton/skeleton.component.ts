f5e1bd789ceed43227413414a8b9a68a5576e4ee
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-18 17:29:35 +0200

    Añadida sidebar en la vista admin y corregida la forma de mostrar los elementos

--- src-frontend/src/app/componentes/vistas/layout/skeleton/skeleton.component.ts
+++ src-frontend/src/app/componentes/vistas/layout/skeleton/skeleton.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { BotonscrollComponent } from '../../../generales/botonscroll/botonscroll.component';
+import { AuthenticationService } from '../../../../servicios/authentication/authentication.service';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skeleton',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, BotonscrollComponent],
+  imports: [HeaderComponent, FooterComponent, BotonscrollComponent, SidebarComponent, CommonModule],
   templateUrl: './skeleton.component.html',
   styleUrl: './skeleton.component.scss'
 })
 export class SkeletonComponent {
+  usuarioLogueado: boolean = false;
 
+  constructor(private authService: AuthenticationService) {}
+
+  ngOnInit(): void {
+    this.authService.usuarioLogueadoCambio.subscribe((estado: boolean) => {
+      this.usuarioLogueado = estado;
+    });
+  }
 }
