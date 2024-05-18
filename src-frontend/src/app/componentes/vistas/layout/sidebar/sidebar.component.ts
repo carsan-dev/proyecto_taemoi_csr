7e3675f6dc28f3c212738226fdfca161bc44cca5
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-18 20:30:21 +0200

    Diseño definitivo de la sidebar con las nuevas funcionalidades maquetadas

--- src-frontend/src/app/componentes/vistas/layout/sidebar/sidebar.component.ts
+++ src-frontend/src/app/componentes/vistas/layout/sidebar/sidebar.component.ts
@@ -1,51 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { SidebarService } from '../../../../servicios/generales/sidebar.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+  @Output() colapsoCambiado = new EventEmitter<boolean>();
   estaColapsado: boolean = false;
-  botonPulsado: boolean = true;
+  private subscription: Subscription = new Subscription;
 
   constructor(private router: Router, private sidebarService: SidebarService) {}
 
   ngOnInit(): void {
-    this.sidebarService.obtenerSubjectAlternable().subscribe(() => {
+    this.subscription = this.sidebarService.obtenerSubjectAlternable().subscribe(() => {
       this.alternarColapso();
     });
   }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
   alternarColapso() {
     this.estaColapsado = !this.estaColapsado;
+    this.colapsoCambiado.emit(this.estaColapsado);
   }
 
-  irAListado() {
-    this.router.navigate(['/alumnos']);
-  }
-
-  irACrear() {
-    this.router.navigate(['/alumnos/crear']);
-  }
-
-  irAEliminar() {
-    this.router.navigate(['/alumnos/eliminar']);
+  irARuta(ruta: string) {
+    this.router.navigate([ruta]);
   }
 
   alternarVisibilidadSidebar(): void {
-    this.sidebarService.alternarSidebar();
-    this.alternarEstadoBotonPulsado()
+    this.alternarColapso();
   }
-
-  alternarEstadoBotonPulsado() {
-    this.botonPulsado = !this.botonPulsado;
-    const icono = document.querySelector('.icono-sidebar');
-    if (icono) {
-    icono.classList.toggle('rotado');
-  }}
 }
