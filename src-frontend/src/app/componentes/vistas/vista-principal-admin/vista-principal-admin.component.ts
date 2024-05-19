dee07bc19bc40ae68b534283814a098f5888fefc
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-19 17:19:30 +0200

    Implementada medida de seguridad en caso de intentar acceder sin el rol adecuado y mensaje de bienvenida personalizado

--- src-frontend/src/app/componentes/vistas/vista-principal-admin/vista-principal-admin.component.ts
+++ src-frontend/src/app/componentes/vistas/vista-principal-admin/vista-principal-admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
+import { AuthenticationService } from '../../../servicios/authentication/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vista-principal-admin',
@@ -10,13 +12,15 @@ import { SidebarComponent } from '../layout/sidebar/sidebar.component';
   styleUrl: './vista-principal-admin.component.scss',
 })
 export class VistaPrincipalAdminComponent implements OnInit {
-  token: string | null = null;
-  tokenExiste: boolean = false;
+  nombreUsuario: string | null = '';
+
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
   ngOnInit(): void {
-    if (typeof localStorage !== 'undefined') {
-      this.token = localStorage.getItem('token');
-      this.tokenExiste = !!this.token;
+    if (!this.authService.tieneRolAdmin() && !this.authService.tieneRolManager()) {
+      this.router.navigate(['/inicio']);
+    } else {
+      this.nombreUsuario = this.authService.obtenerNombreUsuario();
     }
   }
 }
