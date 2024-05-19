e2bd734054cbf1899e884d6e681063441553bb8c
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-19 14:25:02 +0200

    Añadidos todos los endpoints de mi api

--- src-frontend/src/app/servicios/endpoints/endpoints.service.ts
+++ src-frontend/src/app/servicios/endpoints/endpoints.service.ts
@@ -25,6 +25,16 @@ export class EndpointsService {
     });
   }
 
+  obtenerGruposDeAlumno(alumnoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<any>(`${this.urlBase}/alumnos/${alumnoId}/grupos`, {
+      headers: headers,
+    });
+  }
+
   crearAlumno(alumnoData: any, imagen: File | null, token: string): Observable<any> {
     const formData = new FormData();
     const headers = new HttpHeaders({
@@ -39,7 +49,7 @@ export class EndpointsService {
     });
   }
 
-actualizarAlumno(id: number, formData: FormData, token: String): Observable<any> {
+actualizarAlumno(id: number, formData: FormData, token: string): Observable<any> {
   const headers = new HttpHeaders({
     Authorization: `Bearer ${token}`,
   });
@@ -49,7 +59,7 @@ actualizarAlumno(id: number, formData: FormData, token: String): Observable<any>
     });
   }
 
-  eliminarImagenAlumno(id: number, token: String): Observable<any> {
+  eliminarImagenAlumno(id: number, token: string): Observable<any> {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${token}`,
     });
@@ -65,6 +75,166 @@ actualizarAlumno(id: number, formData: FormData, token: String): Observable<any>
     });
 
     return this.http.delete<any>(`${this.urlBase}/alumnos/${id}`, {
+      headers: headers,
+    });
+  }
+
+  obtenerTodosLosGrupos(token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<any>(`${this.urlBase}/grupos`, {
+      headers: headers,
+    });
+  }
+
+  obtenerGrupoPorId(id: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<any>(`${this.urlBase}/grupos/${id}`, {
+      headers: headers,
+    });
+  }
+
+  crearGrupo(grupoData: any, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.post<any>(`${this.urlBase}/grupos`, grupoData, {
+      headers: headers,
+    });
+  }
+
+  actualizarGrupo(id: number, grupoData: any, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.put<any>(`${this.urlBase}/grupos/${id}`, grupoData, {
+      headers: headers,
+    });
+  }
+
+  eliminarGrupo(id: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.delete<any>(`${this.urlBase}/grupos/${id}`, {
+      headers: headers,
+    });
+  }
+
+  agregarAlumnoAGrupo(grupoId: number, alumnoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.post<any>(`${this.urlBase}/grupos/${grupoId}/alumnos/${alumnoId}`, null, {
+      headers: headers,
+    });
+  }
+
+  eliminarAlumnoDeGrupo(grupoId: number, alumnoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.delete<any>(`${this.urlBase}/grupos/${grupoId}/alumnos/${alumnoId}`, {
+      headers: headers,
+    });
+  }
+
+  obtenerTurnosDelGrupo(grupoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<any>(`${this.urlBase}/grupos/${grupoId}/turnos`, {
+      headers: headers,
+    });
+  }
+
+  agregarTurnoAGrupo(grupoId: number, turnoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.post<any>(`${this.urlBase}/grupos/${grupoId}/turnos/${turnoId}`, null, {
+      headers: headers,
+    });
+  }
+
+  eliminarTurnoDeGrupo(grupoId: number, turnoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.delete<any>(`${this.urlBase}/grupos/${grupoId}/turnos/${turnoId}`, {
+      headers: headers,
+    });
+  }
+
+  obtenerTurnos(token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<any>(`${this.urlBase}/turnos`, {
+      headers: headers,
+    });
+  }
+
+  obtenerTurnoPorId(turnoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<any>(`${this.urlBase}/turnos/${turnoId}`, {
+      headers: headers,
+    });
+  }
+
+  crearTurnoSinGrupo(turnoData: any, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.post<any>(`${this.urlBase}/turnos/crear`, turnoData, {
+      headers: headers,
+    });
+  }
+
+  crearTurnoConGrupo(turnoData: any, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.post<any>(`${this.urlBase}/turnos/crear-asignando-grupo`, turnoData, {
+      headers: headers,
+    });
+  }
+
+  actualizarTurno(turnoId: number, turnoData: any, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.put<any>(`${this.urlBase}/turnos/${turnoId}`, turnoData, {
+      headers: headers,
+    });
+  }
+
+  eliminarTurno(turnoId: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.delete<any>(`${this.urlBase}/turnos/${turnoId}`, {
       headers: headers,
     });
   }
