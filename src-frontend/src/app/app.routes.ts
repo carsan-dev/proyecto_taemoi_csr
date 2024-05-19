a13b75bdae566b55d5796e6dbd0be44e8ab5cdd9
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-19 14:28:24 +0200

    Añadidas las nuevas rutas a las diferentes vistas y modificadas las rutas en la sidebar

--- src-frontend/src/app/app.routes.ts
+++ src-frontend/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { VistaLoginComponent } from './componentes/vistas/vista-login/vista-login.component';
 import { EscaparatePrincipalComponent } from './componentes/vistas/escaparate-principal/escaparate-principal.component';
 import { VistaPrincipalAdminComponent } from './componentes/vistas/vista-principal-admin/vista-principal-admin.component';
-import { ListadoAlumnosCompletoDTOComponent } from './componentes/endpoints/listado-alumnos-completo/listado-alumnos-completo.component';
+import { EditarAlumnoComponent } from './componentes/endpoints/editar-alumno/editar-alumno.component';
 import { CrearAlumnoComponent } from './componentes/endpoints/crear-alumno/crear-alumno.component';
 import { EliminarAlumnoComponent } from './componentes/endpoints/eliminar-alumno/eliminar-alumno.component';
 import { EventosComponent } from './componentes/vistas/eventos/eventos.component';
 import { ContactoComponent } from './componentes/vistas/contacto/contacto.component';
 import { EltaekwondoComponent } from './componentes/vistas/eltaekwondo/eltaekwondo.component';
 import { HorariosComponent } from './componentes/vistas/horarios/horarios.component';
+import { ListadoAlumnosComponent } from './componentes/endpoints/listado-alumnos/listado-alumnos.component';
+import { ListadoGruposComponent } from './componentes/endpoints/listado-grupos/listado-grupos.component';
+import { TurnosGrupoComponent } from './componentes/endpoints/listado-grupos/turnos-grupo/turnos-grupo.component';
 
 export const routes: Routes = [
   { path: 'inicio', component: EscaparatePrincipalComponent },
@@ -19,9 +22,12 @@ export const routes: Routes = [
   { path: 'contacto', component: ContactoComponent },
   { path: 'login', component: VistaLoginComponent },
   { path: 'adminpage', component: VistaPrincipalAdminComponent },
-  { path: 'alumnosEditar', component: ListadoAlumnosCompletoDTOComponent },
+  { path: 'alumnosListar', component: ListadoAlumnosComponent },
+  { path: 'alumnosEditar', component: EditarAlumnoComponent },
   { path: 'alumnosCrear', component: CrearAlumnoComponent },
   { path: 'alumnosEliminar', component: EliminarAlumnoComponent },
+  { path: 'gruposListar', component: ListadoGruposComponent },
+  { path: 'turnosGrupo/:id', component: TurnosGrupoComponent },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
 ];
 
