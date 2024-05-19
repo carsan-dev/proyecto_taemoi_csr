a916b26785a63cbbb63886685998888d73f568de
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-19 14:27:29 +0200

    Creado componente listado grupos para listar todos los grupos de la base de datos con opción para visualizar sus turnos asociados

---
+++ src-frontend/src/app/componentes/endpoints/listado-grupos/listado-grupos.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { EndpointsService } from '../../../servicios/endpoints/endpoints.service';
+import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-listado-grupos',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './listado-grupos.component.html',
+  styleUrl: './listado-grupos.component.scss'
+})
+export class ListadoGruposComponent implements OnInit{
+  grupos: any[] = [];
+
+  constructor(private endpointsService: EndpointsService) { }
+
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') {
+    this.obtenerGrupos();
+    }
+  }
+
+  obtenerGrupos() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.endpointsService
+        .obtenerTodosLosGrupos(token)
+        .subscribe({
+          next: (response) => {
+            this.grupos = response;
+          },
+          error: (error) => {
+            Swal.fire({
+              title: 'Error en la petición',
+              text: 'No hemos podido conectar con el servidor',
+              icon: 'error',
+            });
+          },
+        });
+    }
+  }
+}
