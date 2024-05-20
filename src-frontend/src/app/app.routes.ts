e66f16cc3646d4d94ed24fa11572de8feac3cba2
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-20 20:23:44 +0200

    Añadidas las rutas a los nuevos componentes

--- src-frontend/src/app/app.routes.ts
+++ src-frontend/src/app/app.routes.ts
@@ -15,6 +15,9 @@ import { ListadoGruposComponent } from './componentes/endpoints/listado-grupos/l
 import { TurnosGrupoComponent } from './componentes/endpoints/listado-grupos/turnos-grupo/turnos-grupo.component';
 import { VistaPrincipalUserComponent } from './componentes/vistas/vista-principal-user/vista-principal-user.component';
 import { CrearGrupoComponent } from './componentes/endpoints/crear-grupo/crear-grupo.component';
+import { EditarGrupoComponent } from './componentes/endpoints/editar-grupo/editar-grupo.component';
+import { GestionarAlumnosComponent } from './componentes/endpoints/listado-grupos/gestionar-alumnos/gestionar-alumnos.component';
+import { SeleccionarAlumnosComponent } from './componentes/endpoints/listado-grupos/gestionar-alumnos/seleccionar-alumnos/seleccionar-alumnos.component';
 
 export const routes: Routes = [
   { path: 'inicio', component: EscaparatePrincipalComponent },
@@ -31,6 +34,9 @@ export const routes: Routes = [
   { path: 'alumnosEliminar', component: EliminarAlumnoComponent },
   { path: 'gruposListar', component: ListadoGruposComponent },
   { path: 'gruposCrear', component: CrearGrupoComponent },
+  { path: 'gruposEditar/:id', component: EditarGrupoComponent },
+  { path: 'gestionarAlumnos/:id', component: GestionarAlumnosComponent },
+  { path: 'seleccionarAlumnos/:id', component: SeleccionarAlumnosComponent },
   { path: 'turnosGrupo/:id', component: TurnosGrupoComponent },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
 ];
