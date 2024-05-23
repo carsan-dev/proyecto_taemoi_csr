9f0f20290f918b4e8afbae10da7edf7654d0d4e1
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-23 11:30:35 +0200

    Implementado endpoint para obtener el usuario autentificado junto a su alumno asignado

--- src-api/src/main/java/com/taemoi/project/controladores/AuthenticationController.java
+++ src-api/src/main/java/com/taemoi/project/controladores/AuthenticationController.java
@@ -1,10 +1,12 @@
 package com.taemoi.project.controladores;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.taemoi.project.dtos.request.LoginRequest;
 import com.taemoi.project.dtos.request.RegistroRequest;
+import com.taemoi.project.dtos.response.AlumnoParaUsuarioDTO;
 import com.taemoi.project.dtos.response.JwtAuthenticationResponse;
+import com.taemoi.project.dtos.response.UsuarioConAlumnoAsociadoDTO;
 import com.taemoi.project.entidades.Usuario;
 import com.taemoi.project.servicios.AuthenticationService;
+import com.taemoi.project.servicios.UsuarioService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,6 +39,8 @@ public class AuthenticationController {
      * Inyección del servicio de autenticación.
      */
 	@Autowired AuthenticationService authenticationService;
+	
+	@Autowired UsuarioService usuarioService;
 
     /**
      * Registra un nuevo usuario en el sistema.
@@ -65,9 +72,32 @@ public class AuthenticationController {
 	 * @return Conjunto de cadenas que representan los roles del usuario.
 	 */
     @GetMapping("/roles")
+	@PreAuthorize("hasRole('ROLE_MANAGER') || hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
     public Set<String> obtenerRoles() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Usuario usuario = (Usuario) authentication.getPrincipal();
         return usuario.getRoles().stream().map(Enum::name).collect(Collectors.toSet());
     }
+    
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('ROLE_MANAGER') || hasRole('ROLE_ADMIN') || hasRole('ROLE_USER')")
+    public UsuarioConAlumnoAsociadoDTO obtenerUsuarioAutenticado() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        Optional<Usuario> usuarioOptional = usuarioService.encontrarPorEmail(email);
+        if (usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+            UsuarioConAlumnoAsociadoDTO usuarioDTO = new UsuarioConAlumnoAsociadoDTO();
+            usuarioDTO.setId(usuario.getId());
+            usuarioDTO.setEmail(usuario.getEmail());
+
+            AlumnoParaUsuarioDTO alumnoUsuarioDTO = AlumnoParaUsuarioDTO.deAlumno(usuario.getAlumno());
+            usuarioDTO.setAlumnoDTO(alumnoUsuarioDTO);
+            
+            return usuarioDTO;
+        } else {
+            return null;
+        }
+    }
+
 }
