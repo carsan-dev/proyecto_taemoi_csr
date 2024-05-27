50efb8b953ee9eb268100f1c7c0d3cd09b72ef72
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-27 23:15:53 +0200

    Implementado el listado de eventos de forma dinámica con los estilos aplicados

--- src-frontend/src/app/componentes/vistas/eventos/eventos.component.ts
+++ src-frontend/src/app/componentes/vistas/eventos/eventos.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { EventosInteface } from '../../../interfaces/eventos-inteface';
 import { AuthenticationService } from '../../../servicios/authentication/authentication.service';
-import { EventosService } from '../../../servicios/generales/eventos.service';
+import { EndpointsService } from '../../../servicios/endpoints/endpoints.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-eventos',
@@ -12,17 +12,31 @@ import { EventosService } from '../../../servicios/generales/eventos.service';
   styleUrl: './eventos.component.scss'
 })
 export class EventosComponent implements OnInit {
-  eventos: EventosInteface[] = [];
+  eventos: any[] = [];
   usuarioLogueado: boolean = false;
 
-  constructor(private authService: AuthenticationService, private eventosService: EventosService) { }
+  constructor(private authService: AuthenticationService, private endpointsService: EndpointsService) { }
 
   ngOnInit(): void {
     this.usuarioLogueado = this.authService.comprobarLogueado();
     this.authService.usuarioLogueadoCambio.subscribe((estado: boolean) => {
       this.usuarioLogueado = estado;
     });
-    this.eventos = this.eventosService.obtenerEventos();
+    this.obtenerEventos();
   }
 
+  obtenerEventos() {
+    this.endpointsService.obtenerEventos().subscribe({
+      next: (response) => {
+        this.eventos = response;
+      },
+      error: (error) => {
+        Swal.fire({
+          title: 'Error',
+          text: 'No hemos podido obtener los eventos.',
+          icon: 'error',
+        });
+      },
+    });
+  }
 }
