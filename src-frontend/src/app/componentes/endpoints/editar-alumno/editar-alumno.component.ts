d020ba2df93ca7c30ed617027708ca7993c0f8ef
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-29 23:40:39 +0200

    Añadido a editar alumno component una preview de la imagen para actualizarla cuando se añada o se quite en la edición

--- src-frontend/src/app/componentes/endpoints/editar-alumno/editar-alumno.component.ts
+++ src-frontend/src/app/componentes/endpoints/editar-alumno/editar-alumno.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { EndpointsService } from '../../../servicios/endpoints/endpoints.service';
 import { CommonModule } from '@angular/common';
 import Swal from 'sweetalert2';
@@ -12,7 +12,7 @@ import { PaginacionComponent } from '../../generales/paginacion/paginacion.compo
   standalone: true,
   imports: [CommonModule, FormsModule, PaginacionComponent],
   templateUrl: './editar-alumno.component.html',
-  styleUrl: './editar-alumno.component.scss'
+  styleUrl: './editar-alumno.component.scss',
 })
 export class EditarAlumnoComponent {
   alumnos: any[] = [];
@@ -28,7 +28,8 @@ export class EditarAlumnoComponent {
 
   tiposTarifa = Object.values(TipoTarifa);
   tiposGrado = Object.values(TipoGrado);
-  inputFile: any;
+  @ViewChild('inputFile', { static: false }) inputFile!: ElementRef;
+  imagenPreview: string | null = null;
 
   constructor(private endpointsService: EndpointsService) {}
 
@@ -73,9 +74,7 @@ export class EditarAlumnoComponent {
     }
 
     if (token) {
-    this.endpointsService
-      .actualizarAlumno(id, formData, token)
-      .subscribe({
+      this.endpointsService.actualizarAlumno(id, formData, token).subscribe({
         next: (response) => {
           Swal.fire({
             title: '¡Bien!',
@@ -98,41 +97,46 @@ export class EditarAlumnoComponent {
   eliminarFoto(id: number) {
     const token = localStorage.getItem('token');
     if (token) {
-    this.endpointsService.eliminarImagenAlumno(id, token).subscribe({
-      next: (response) => {
-        Swal.fire({
-          title: '¡Eliminada!',
-          text: '¡Imagen eliminada correctamente!',
-          icon: 'success',
-        });
-        this.inputFile.nativeElement.value = '';
-        this.obtenerAlumnos();
-      },
+      this.endpointsService.eliminarImagenAlumno(id, token).subscribe({
+        next: (response) => {
+          this.inputFile.nativeElement.value = '';
+          this.alumnoEditado.fotoAlumno = null;
+          this.imagenPreview = 'assets/media/default.webp';
+          this.obtenerAlumnos();
+        },
 
-      error: (error) => {
-        Swal.fire({
-          title: 'Error al actualizar',
-          text: 'Error al actualizar al alumno',
-          icon: 'error',
-        });
-      },
-    });
+        error: (error) => {
+          Swal.fire({
+            title: 'Error al actualizar',
+            text: 'Error al actualizar al alumno',
+            icon: 'error',
+          });
+        },
+      });
+    }
   }
-}
 
-cambiarPagina(pageNumber: number): void {
-  this.paginaActual = pageNumber;
-  this.obtenerAlumnos();
-}
+  cambiarPagina(pageNumber: number): void {
+    this.paginaActual = pageNumber;
+    this.obtenerAlumnos();
+  }
 
   alternarFormulario(alumno: any): void {
     this.mostrarFormulario = !this.mostrarFormulario;
     this.alumnoEditado = { ...alumno };
+    this.imagenPreview = alumno.fotoAlumno
+      ? 'data:' + alumno.fotoAlumno.tipo + ';base64,' + alumno.fotoAlumno.datos
+      : 'assets/media/default.webp';
   }
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
     this.alumnoEditado.fotoAlumno = file;
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.imagenPreview = e.target.result;
+    };
+    reader.readAsDataURL(file);
   }
 }
-
