bc3c35866ca2e5fd7204a084390a518562408449
csanrom1702 <csanrom1702@g.educaand.es>
2024-05-30 19:46:18 +0200

    Corrección del guard para comprobar los roles correctamente al entrar a las vistas protegidas

--- src-frontend/src/app/guards/role.guard.ts
+++ src-frontend/src/app/guards/role.guard.ts
@@ -1,20 +1,25 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from '../servicios/authentication/authentication.service';
 import { inject } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 export const roleGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
   const router = inject(Router);
 
-  return authService.rolesCambio.pipe(
-    map((roles) => {
+  return authService.getRoles().pipe(
+    switchMap((roles) => {
       if (roles.includes('ROLE_ADMIN') || roles.includes('ROLE_MANAGER')) {
-        return true;
+        return of(true);
       } else {
         router.navigate(['/inicio']);
-        return false;
+        return of(false);
       }
+    }),
+    catchError(() => {
+      router.navigate(['/inicio']);
+      return of(false);
     })
   );
 };
