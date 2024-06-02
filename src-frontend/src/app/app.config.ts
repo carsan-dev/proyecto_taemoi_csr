4c074c3c291544fd7e1b442898ee2aa84f8fbb4f
csanrom1702 <csanrom1702@g.educaand.es>
2024-06-02 13:20:22 +0200

    Configurada la pantalla de carga para poder usarla en angular

--- src-frontend/src/app/app.config.ts
+++ src-frontend/src/app/app.config.ts
@@ -5,7 +5,14 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HammerModule } from '@angular/platform-browser';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimationsAsync(), importProvidersFrom(HammerModule)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    importProvidersFrom(HammerModule),
+    importProvidersFrom(NgxSpinnerModule.forRoot()),
+  ],
 };
