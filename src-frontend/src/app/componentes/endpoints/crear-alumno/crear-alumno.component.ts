a4b09737fcd0fff0442bee48134603090d4bf430
csanrom1702 <csanrom1702@g.educaand.es>
2024-06-04 23:05:07 +0200

    Estilos corregidos y validaciones al crear alumnos

--- src-frontend/src/app/componentes/endpoints/crear-alumno/crear-alumno.component.ts
+++ src-frontend/src/app/componentes/endpoints/crear-alumno/crear-alumno.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { EndpointsService } from '../../../servicios/endpoints/endpoints.service';
 import Swal from 'sweetalert2';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TipoTarifa } from '../../../enums/tipo-tarifa';
 import { Router } from '@angular/router';
@@ -13,13 +19,16 @@ import { Router } from '@angular/router';
   templateUrl: './crear-alumno.component.html',
   styleUrl: './crear-alumno.component.scss',
 })
-
 export class CrearAlumnoComponent implements OnInit {
   alumnoData!: FormGroup;
   imagen: File | null = null;
   tiposTarifa = Object.values(TipoTarifa);
 
-  constructor(private fb: FormBuilder, private endpointsService: EndpointsService, private router: Router) { }
+  constructor(
+    private fb: FormBuilder,
+    private endpointsService: EndpointsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.initForm();
@@ -29,15 +38,21 @@ export class CrearAlumnoComponent implements OnInit {
     this.alumnoData = this.fb.group({
       nombre: ['', Validators.required],
       apellidos: ['', Validators.required],
+      numeroExpediente: [
+        '',
+        Validators.required,
+        Validators.pattern('^[0-9]+$'),
+      ],
       fechaNacimiento: ['', Validators.required],
-      numeroExpediente: ['', Validators.required],
-      nif: ['', Validators.required],
+      nif: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]{8}[A-Za-z]$')],
+      ],
       direccion: ['', Validators.required],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
       email: ['', [Validators.required, Validators.email]],
-      telefono: ['', Validators.required],
       tipoTarifa: ['', Validators.required],
       fechaAlta: ['', Validators.required],
-      fechaBaja: ['']
     });
   }
 
@@ -46,31 +61,33 @@ export class CrearAlumnoComponent implements OnInit {
     const alumnoData = this.alumnoData.value;
 
     if (token) {
-    this.endpointsService.crearAlumno(alumnoData, this.imagen, token).subscribe({
-      next: (response) => {
-        Swal.fire({
-          title: 'Perfecto!',
-          text: 'Has creado un nuevo alumno',
-          icon: 'success',
+      this.endpointsService
+        .crearAlumno(alumnoData, this.imagen, token)
+        .subscribe({
+          next: (response) => {
+            Swal.fire({
+              title: 'Perfecto!',
+              text: 'Has creado un nuevo alumno',
+              icon: 'success',
+            });
+            this.router.navigate(['/alumnosListar']);
+          },
+          error: (error) => {
+            Swal.fire({
+              title: 'Error en la petición',
+              text: 'No has completado todos los campos requeridos',
+              icon: 'error',
+            });
+          },
+          complete: () => {},
         });
-        this.router.navigate(['/alumnosListar']);
-      },
-      error: (error) => {
-        Swal.fire({
-          title: 'Error en la petición',
-          text: 'No has completado todos los campos requeridos',
-          icon: 'error',
-        });
-      },
-      complete: () => {},
-    });
+    }
   }
-}
 
-onFileChange(event: any) {
-  const fileList: FileList = event.target.files;
-  if (fileList.length > 0) {
-    this.imagen = fileList[0];
+  onFileChange(event: any) {
+    const fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      this.imagen = fileList[0];
+    }
   }
-}
 }
