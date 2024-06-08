ea933cf4f11b23022842e8589ca908a730e06fa4
csanrom1702 <csanrom1702@g.educaand.es>
2024-06-08 14:18:36 +0200

    Modificado main para cargar las variables de entorno

--- src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
+++ src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
@@ -3,10 +3,16 @@ package com.taemoi.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class TaeMoiApplication {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(TaeMoiApplication.class, args);
 	}
 }
