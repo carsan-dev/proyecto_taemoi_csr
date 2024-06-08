c55a6def7c84f2cf920025655ef7a2161c060bb6
csanrom1702 <csanrom1702@g.educaand.es>
2024-06-08 17:30:17 +0200

    Arreglado el main para buscar el archivo en cualquier parte del sistema

--- src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
+++ src-api/src/main/java/com/taemoi/project/TaeMoiApplication.java
@@ -1,5 +1,10 @@
 package com.taemoi.project;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +13,33 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class TaeMoiApplication {
 
-	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
-		SpringApplication.run(TaeMoiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        int maxDepth = 5;
+        Optional<Path> envPathOptional = findEnvFile(Paths.get("").toAbsolutePath(), maxDepth);
+        if (envPathOptional.isPresent()) {
+            Path envPath = envPathOptional.get();
+            Dotenv dotenv = Dotenv.configure().directory(envPath.getParent().toString()).load();
+            dotenv.entries().forEach(entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+            });
+        } else {
+            System.out.println(".env file not found!");
+        }
+
+        SpringApplication.run(TaeMoiApplication.class, args);
+    }
+
+    private static Optional<Path> findEnvFile(Path startPath, int maxDepth) {
+        Path currentPath = startPath;
+        int depth = 0;
+        while (currentPath != null && depth <= maxDepth) {
+            Path envFile = currentPath.resolve(".env");
+            if (Files.exists(envFile)) {
+                return Optional.of(envFile);
+            }
+            currentPath = currentPath.getParent();
+            depth++;
+        }
+        return Optional.empty();
+    }
 }
