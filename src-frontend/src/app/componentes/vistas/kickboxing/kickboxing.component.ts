8bd5a40331aa5fbad09a18c6221bf02d2d3de3ab
csanrom1702 <csanrom1702@g.educaand.es>
2024-10-10 17:48:02 +0200

    Creados los nuevos componentes para kickboxing y pilates

---
+++ src-frontend/src/app/componentes/vistas/kickboxing/kickboxing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kickboxing',
+  standalone: true,
+  imports: [],
+  templateUrl: './kickboxing.component.html',
+  styleUrl: './kickboxing.component.scss'
+})
+export class KickboxingComponent {
+
+}
---
+++ src-frontend/src/app/componentes/vistas/pilates/pilates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pilates',
+  standalone: true,
+  imports: [],
+  templateUrl: './pilates.component.html',
+  styleUrl: './pilates.component.scss'
+})
+export class PilatesComponent {
+
+}
