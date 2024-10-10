75bad32bbe5ac4bf706aa8e6942f5bf5d9cf54f6
csanrom1702 <csanrom1702@g.educaand.es>
2024-10-10 22:48:46 +0200

    Diseño y contenido beta del componente kickboxing

--- src-frontend/src/app/componentes/vistas/kickboxing/kickboxing.component.ts
+++ src-frontend/src/app/componentes/vistas/kickboxing/kickboxing.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChildren, QueryList, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AuthenticationService } from '../../../servicios/authentication/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-kickboxing',
   standalone: true,
   imports: [],
   templateUrl: './kickboxing.component.html',
-  styleUrl: './kickboxing.component.scss'
+  styleUrls: ['./kickboxing.component.scss']
 })
-export class KickboxingComponent {
+export class KickboxingComponent implements OnInit, AfterViewInit {
+  usuarioLogueado: boolean = false;
 
+  @ViewChildren('animatedImage')
+  images!: QueryList<ElementRef>;
+
+  constructor(
+    private readonly authService: AuthenticationService,
+    private readonly router: Router,
+    @Inject(PLATFORM_ID) private readonly platformId: Object
+  ) { }
+
+  ngOnInit(): void {
+    this.usuarioLogueado = this.authService.comprobarLogueado();
+    this.authService.usuarioLogueadoCambio.subscribe((estado: boolean) => {
+      this.usuarioLogueado = estado;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+      };
+
+      const observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
+
+      this.images.forEach(image => {
+        observer.observe(image.nativeElement);
+      });
+    }
+  }
+
+  handleIntersection(entries: IntersectionObserverEntry[], observer: IntersectionObserver): void {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        (entry.target as HTMLElement).classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+
+  irARuta(ruta: string): void {
+    this.router.navigate([ruta]);
+  }
 }
