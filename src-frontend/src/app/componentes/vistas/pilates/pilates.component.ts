24e9f2a3bc1025c7aa98eb74502dd16e4f43a43b
carsan-dev <crolyx16@gmail.com>
2024-10-12 02:05:10 +0200

    Mejoras en el estilo de los componentes de las vistas de deportes e implementado el de pilates

--- src-frontend/src/app/componentes/vistas/pilates/pilates.component.ts
+++ src-frontend/src/app/componentes/vistas/pilates/pilates.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChildren, QueryList, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AuthenticationService } from '../../../servicios/authentication/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pilates',
@@ -7,6 +10,51 @@ import { Component } from '@angular/core';
   templateUrl: './pilates.component.html',
   styleUrl: './pilates.component.scss'
 })
-export class PilatesComponent {
+export class PilatesComponent implements OnInit, AfterViewInit {
+  usuarioLogueado: boolean = false;
 
+  @ViewChildren('animatedImage')
+  images!: QueryList<ElementRef>;
+
+  constructor(
+    private readonly authService: AuthenticationService,
+    private readonly router: Router,
+    @Inject(PLATFORM_ID) private readonly platformId: Object
+  ) { }
+
+  ngOnInit(): void {
+    this.usuarioLogueado = this.authService.comprobarLogueado();
+    this.authService.usuarioLogueadoCambio.subscribe((estado: boolean) => {
+      this.usuarioLogueado = estado;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+      };
+
+      const observer = new IntersectionObserver(this.handleIntersection.bind(this), options);
+
+      this.images.forEach(image => {
+        observer.observe(image.nativeElement);
+      });
+    }
+  }
+
+  handleIntersection(entries: IntersectionObserverEntry[], observer: IntersectionObserver): void {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        (entry.target as HTMLElement).classList.add('fade-in');
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+
+  irARuta(ruta: string) {
+    this.router.navigate([ruta]);
+  }
 }
