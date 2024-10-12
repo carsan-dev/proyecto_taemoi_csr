8b0c5a2a6ea65df414b9a5b8adf9de8461f79ea6
carsan-dev <crolyx16@gmail.com>
2024-10-12 22:01:19 +0200

    Reprogramado el slider para convertirse en un banner que muestra los eventos de la base de datos

--- src-frontend/src/app/componentes/generales/carousel/slider-tocable/slider-tocable.component.ts
+++ src-frontend/src/app/componentes/generales/carousel/slider-tocable/slider-tocable.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { ImagenInterface } from '../../../../interfaces/imagen-interface';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { AuthenticationService } from '../../../../servicios/authentication/authentication.service';
+import { EndpointsService } from '../../../../servicios/endpoints/endpoints.service';
 
 @Component({
   selector: 'app-slider-tocable',
@@ -9,20 +11,44 @@ import { ImagenInterface } from '../../../../interfaces/imagen-interface';
   templateUrl: './slider-tocable.component.html',
   styleUrl: './slider-tocable.component.scss'
 })
-export class SliderTocableComponent {
-  @Input() imagenesSlider: ImagenInterface[] = [];
+export class SliderTocableComponent implements OnInit {
+  eventos: any[] = [];
+  defaultFotos: any[] = [
+    { titulo: 'Evento 1', descripcion: 'Descripción del evento 1', fotoEvento: { tipo: 'image/webp', datos: '../../../../assets/media/default.webp' } },
+    { titulo: 'Evento 2', descripcion: 'Descripción del evento 2', fotoEvento: { tipo: 'image/webp', datos: '../../../../assets/media/default.webp' } },
+    { titulo: 'Evento 3', descripcion: 'Descripción del evento 3', fotoEvento: { tipo: 'image/webp', datos: '../../../../assets/media/default.webp' } },
+    { titulo: 'Evento 4', descripcion: 'Descripción del evento 4', fotoEvento: { tipo: 'image/webp', datos: '../../../../assets/media/default.webp' } },
+    { titulo: 'Evento 5', descripcion: 'Descripción del evento 5', fotoEvento: { tipo: 'image/webp', datos: '../../../../assets/media/default.webp' } }
+  ];
 
-  indexSeleccionado: number = 0;
+  usuarioLogueado: boolean = false;
 
-  mostrarAnterior(i: number) {
-    if (this.indexSeleccionado > 0) {
-      this.indexSeleccionado = i - 1;
-    }
+  constructor(
+    private authService: AuthenticationService,
+    private endpointsService: EndpointsService
+  ) {}
+
+  ngOnInit(): void {
+    this.usuarioLogueado = this.authService.comprobarLogueado();
+    this.authService.usuarioLogueadoCambio.subscribe((estado: boolean) => {
+      this.usuarioLogueado = estado;
+    });
+    this.obtenerEventos();
   }
 
-  mostrarSiguiente(i: number) {
-    if (this.indexSeleccionado < this.imagenesSlider?.length - 1) {
-      this.indexSeleccionado = i + 1;
-    }
+  obtenerEventos() {
+    this.endpointsService.obtenerEventos().subscribe({
+      next: (response) => {
+        this.eventos = response.length ? response.slice(0, 5) : this.defaultFotos; // Mostrar fotos predeterminadas si no hay eventos
+      },
+      error: (error) => {
+        Swal.fire({
+          title: 'Error',
+          text: 'No hemos podido obtener los eventos.',
+          icon: 'error',
+        });
+        this.eventos = this.defaultFotos; // Mostrar fotos predeterminadas en caso de error
+      },
+    });
   }
 }
