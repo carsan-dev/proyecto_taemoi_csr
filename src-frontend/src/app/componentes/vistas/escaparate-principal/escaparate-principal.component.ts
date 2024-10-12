bfe912e8c33a7c789415d2277babfb68bb0cc9b7
carsan-dev <crolyx16@gmail.com>
2024-10-12 22:02:02 +0200

    Usado el nuevo banner y cambiados colores de los botones

--- src-frontend/src/app/componentes/vistas/escaparate-principal/escaparate-principal.component.ts
+++ src-frontend/src/app/componentes/vistas/escaparate-principal/escaparate-principal.component.ts
@@ -2,7 +2,6 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  OnInit,
   ViewChild,
   ViewChildren, 
   QueryList, 
@@ -15,6 +14,7 @@ import { SliderTocableComponent } from '../../generales/carousel/slider-tocable/
 import { HammerModule } from '@angular/platform-browser';
 import { MapaComponent } from '../../generales/mapa/mapa.component';
 import { Router } from '@angular/router';
+import { EndpointsService } from '../../../servicios/endpoints/endpoints.service'; // Importar el servicio de eventos
 
 @Component({
   selector: 'app-escaparate-principal',
@@ -23,7 +23,7 @@ import { Router } from '@angular/router';
   templateUrl: './escaparate-principal.component.html',
   styleUrl: './escaparate-principal.component.scss',
 })
-export class EscaparatePrincipalComponent implements OnInit, AfterViewInit {
+export class EscaparatePrincipalComponent implements AfterViewInit {
   @ViewChild('videoPresentacion')
   videoPresentacion!: ElementRef<HTMLVideoElement>;
 
@@ -31,59 +31,13 @@ export class EscaparatePrincipalComponent implements OnInit, AfterViewInit {
   images!: QueryList<ElementRef>;
 
   usuarioLogueado: boolean = false;
-  eventoActualIndex: number = 0;
-  idIntervalo: any;
-
-  imagenesSlider = [
-    {
-      imgSrc: '../../../../assets/media/dia_madre.webp',
-      imgAlt:
-        'Imagen que muestra un entrenamiento especial con motivo del día de la madre en el que se ven a los hijos y sus madres entrenando juntos.',
-      captionTexto:
-        'Celebra el día de la madre con un entrenamiento especial para fortalecer vínculos familiares',
-    },
-    {
-      imgSrc: '../../../../assets/media/dia_padre.webp',
-      imgAlt:
-        'Imagen que muestra un entrenamiento especial con motivo del día del padre en el que se ven a los hijos y sus padres entrenando juntos.',
-      captionTexto:
-        'Únete a nuestro entrenamiento especial por el día del padre y crea recuerdos inolvidables.',
-    },
-    {
-      imgSrc: '../../../../assets/media/estiramientos.webp',
-      imgAlt:
-        'Imagen que muestra una dinámica de estiramientos en un día de clase normal.',
-      captionTexto:
-        'Relaja tu cuerpo y fortalece tu mente con nuestra sesión de estiramientos al final de cada clase.',
-    },
-    {
-      imgSrc: '../../../../assets/media/reunion_tatami.webp',
-      imgAlt:
-        'Imagen que muestra a todos los alumnos haciendo un círculo y colaborando juntos.',
-      captionTexto:
-        'Experimenta la fuerza de la comunidad en nuestro club participando en las dinámicas de grupo.',
-    },
-    {
-      imgSrc: '../../../../assets/media/sara_competicion.webp',
-      imgAlt:
-        'Imagen que muestra a una de nuestras competidoras en una competición con motivo del día de la mujer.',
-      captionTexto:
-        'Apúntate al equipo de competición para llegar a lo más alto y aprender por el camino.',
-    },
-  ];
 
   constructor(
     private readonly authService: AuthenticationService,
     private readonly router: Router,
+    private readonly endpointsService: EndpointsService, // Inyectar el servicio de eventos
     @Inject(PLATFORM_ID) private readonly platformId: Object
   ) {}
-
-  ngOnInit(): void {
-    this.usuarioLogueado = this.authService.comprobarLogueado();
-    this.authService.usuarioLogueadoCambio.subscribe((estado: boolean) => {
-      this.usuarioLogueado = estado;
-    });
-  }
 
   ngAfterViewInit(): void {
     const video: HTMLVideoElement = this.videoPresentacion.nativeElement;
