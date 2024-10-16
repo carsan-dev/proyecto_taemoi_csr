63c4c1cf6f50e1b234068ef8572ade9ea3ba6797
carsan-dev <crolyx16@gmail.com>
2024-10-17 00:07:21 +0200

    Eliminado hammerModule de la app y configurada la variable de producción para que la url sea la de desarrollo temporalmente

--- src-frontend/src/app/app.config.ts
+++ src-frontend/src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HammerModule } from '@angular/platform-browser';
 import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
-    importProvidersFrom(HammerModule),
     importProvidersFrom(NgxSpinnerModule.forRoot()),
   ],
 };
--- src-frontend/src/environments/environment.prod.ts
+++ src-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://taemoi.us-east-1.elasticbeanstalk.com/api'
+  apiUrl: 'http://localhost:8080/api'
 };
