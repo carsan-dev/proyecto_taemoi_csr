ff1c8322ec08b41028a89d36af07561050a7f19d
carsan-dev <crolyx16@gmail.com>
2024-11-27 17:04:54 +0100

    Añadidos métodos para filtrar y ordenar los productos y terminados de implementar todo lo relacionado con productos

--- src-api/src/main/java/com/taemoi/project/servicios/ProductoService.java
+++ src-api/src/main/java/com/taemoi/project/servicios/ProductoService.java
@@ -3,13 +3,20 @@ package com.taemoi.project.servicios;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.taemoi.project.entidades.Producto;
 
 public interface ProductoService {
 
     List<Producto> obtenerTodosLosProductos();
+    
+    Page<Producto> obtenerProductosPaginados(Pageable pageable);
 
-    Optional<Producto> obtenerProductoPorId(Long id);
+    Optional<Producto> obtenerProductoPorId(Long id); 
+
+	Page<Producto> buscarProductosPorConcepto(String concepto, Pageable pageable);
     
     Producto guardarProducto(Producto producto);
     
