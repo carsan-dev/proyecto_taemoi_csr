f44c2a91ab38ea41df1736c0497812b5a483a880
carsan-dev <crolyx16@gmail.com>
2024-12-19 14:53:16 +0100

    Mejorado diseño del modal con los alumnos de cada deporte y añadida opción para abrir su vista edición al hacer click

--- src-frontend/src/app/componentes/generales/grupo-alumnos-modal/grupo-alumnos-modal.component.ts
+++ src-frontend/src/app/componentes/generales/grupo-alumnos-modal/grupo-alumnos-modal.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-grupo-alumnos-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './grupo-alumnos-modal.component.html',
   styleUrl: './grupo-alumnos-modal.component.scss'
 })
