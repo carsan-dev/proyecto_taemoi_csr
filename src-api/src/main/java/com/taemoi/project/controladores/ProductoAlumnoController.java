a38544a03f56b39df11fe240da4a926eaf08f353
carsan-dev <crolyx16@gmail.com>
2025-01-14 14:38:40 +0100

    Implementada la carga de mensualidades tanto general como individual y manejadas excepciones necesarias

--- src-api/src/main/java/com/taemoi/project/controladores/ProductoAlumnoController.java
+++ src-api/src/main/java/com/taemoi/project/controladores/ProductoAlumnoController.java
@@ -2,6 +2,7 @@ package com.taemoi.project.controladores;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -73,6 +74,32 @@ public class ProductoAlumnoController {
 			return ResponseEntity.ok(reserva);
 		} catch (IllegalStateException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+		}
+	}
+	
+	@PostMapping("/mensualidades/general")
+	@PreAuthorize("hasRole('ROLE_MANAGER') || hasRole('ROLE_ADMIN')")
+	public ResponseEntity<?> cargarMensualidadesGenerales(@RequestBody String nombreMensualidad) {
+		productoAlumnoService.cargarMensualidadesGenerales(nombreMensualidad);
+		return ResponseEntity.ok(Map.of("mensaje", "Mensualidades creadas correctamente."));
+	}
+
+	@PostMapping("/mensualidades/individual")
+	@PreAuthorize("hasRole('ROLE_MANAGER') || hasRole('ROLE_ADMIN')")
+	public ResponseEntity<?> cargarMensualidadIndividual(@RequestParam Long alumnoId,
+			@RequestBody String nombreMensualidad, @RequestParam(defaultValue = "false") boolean forzar) {
+		try {
+			productoAlumnoService.cargarMensualidadIndividual(alumnoId, nombreMensualidad, forzar);
+			return ResponseEntity.ok(Map.of("mensaje", "Mensualidad individual creada correctamente."));
+		} catch (IllegalStateException ex) {
+			// Devuelve un código 409 con un mensaje específico
+			return ResponseEntity.status(HttpStatus.CONFLICT)
+					.body(Map.of("mensaje", ex.getMessage(), "accion", "confirmar" // Indica que es necesario confirmar
+					));
+		} catch (Exception ex) {
+			// Manejo genérico de errores
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(Map.of("mensaje", "Ocurrió un error al crear la mensualidad.", "detalle", ex.getMessage()));
 		}
 	}
 }
