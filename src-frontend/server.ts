7e571248676168d720d6aed8600bf5e53a9d7f7e
csanrom1702 <csanrom1702@g.educaand.es>
2025-10-21 20:04:22 +0200

    Actualizado Java a la versión 21 y Angular a la 20, migrando las funcionalidades (corregir dependencias deprecated). Corregidos dockerfile con nuevas versiones y actualizadas dependencias de angular

--- src-frontend/server.ts
+++ src-frontend/server.ts
@@ -1,18 +1,16 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
+import { AngularNodeAppEngine, createNodeRequestHandler, writeResponseToNodeResponse } from '@angular/ssr/node';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
-import bootstrap from './src/main.server';
+import { dirname, resolve } from 'node:path';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  const commonEngine = new CommonEngine();
+  const angularNodeAppEngine = new AngularNodeAppEngine();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -26,18 +24,16 @@ export function app(): express.Express {
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+    angularNodeAppEngine
+      .handle(req)
+      .then((response) => {
+        if (response) {
+          writeResponseToNodeResponse(response, res);
+        } else {
+          next();
+        }
       })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+      .catch(next);
   });
 
   return server;
