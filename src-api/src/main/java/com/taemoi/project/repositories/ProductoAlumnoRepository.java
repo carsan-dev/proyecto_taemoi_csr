18bde78118cb8fb945901466934d230c8632ab67
carsan-dev <crolyx16@gmail.com>
2025-11-07 14:23:02 +0100

    Modificado backend para añadir documento de listado de deudas para generar en pdf o en csv

--- src-api/src/main/java/com/taemoi/project/repositories/ProductoAlumnoRepository.java
+++ src-api/src/main/java/com/taemoi/project/repositories/ProductoAlumnoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.taemoi.project.entities.ProductoAlumno;
 
@@ -11,6 +12,9 @@ public interface ProductoAlumnoRepository extends JpaRepository<ProductoAlumno, 
 	List<ProductoAlumno> findByAlumnoId(Long alumnoId);
 
 	Optional<ProductoAlumno> findByAlumnoIdAndProductoId(Long id, Long id2);
-	
+
 	boolean existsByConcepto(String concepto);
+
+	@Query("SELECT pa FROM ProductoAlumno pa JOIN FETCH pa.alumno a WHERE pa.pagado = false ORDER BY a.nombre, a.apellidos, pa.fechaAsignacion")
+	List<ProductoAlumno> findAllUnpaidWithAlumno();
 }
