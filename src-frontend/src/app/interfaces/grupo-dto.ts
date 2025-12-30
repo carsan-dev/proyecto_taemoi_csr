7cd99084777cc084396a3b95092476d9e2bead1e
carsan-dev <crolyx16@gmail.com>
2025-12-30 22:25:07 +0100

    Cambiado dto de grupo para añadir el rango

--- src-frontend/src/app/interfaces/grupo-dto.ts
+++ src-frontend/src/app/interfaces/grupo-dto.ts
@@ -3,5 +3,8 @@ import { AlumnoDTO } from "./alumno-dto";
 export interface GrupoDTO {
   id: number;
   nombre: string;
+  deporte?: string;
+  rangoEdadMin?: number | null;
+  rangoEdadMax?: number | null;
   alumnos: AlumnoDTO[];
 }
