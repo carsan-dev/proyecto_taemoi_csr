6a6ec262fd7400bca5368e45a9774f20f8ac6a4a
carsan-dev <crolyx16@gmail.com>
2026-01-17 16:20:55 +0100

    Creado dto a usar para las observaciones

---
+++ src-api/src/main/java/com/taemoi/project/dtos/request/AlumnoObservacionesDTO.java
@@ -0,0 +1,16 @@
+package com.taemoi.project.dtos.request;
+
+public class AlumnoObservacionesDTO {
+	private String observaciones;
+
+	public AlumnoObservacionesDTO() {
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+}
