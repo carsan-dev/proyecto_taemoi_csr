b88cc44535b67ee21848dc28af0a5e99966dfa54
carsan-dev <crolyx16@gmail.com>
2026-01-21 20:27:21 +0100

    Añadido head a las imagenes de eventos para intentar que no lleguen grises

--- src-api/src/main/java/com/taemoi/project/config/SecurityConfiguration.java
+++ src-api/src/main/java/com/taemoi/project/config/SecurityConfiguration.java
@@ -158,6 +158,7 @@ public class SecurityConfiguration {
 						.requestMatchers(HttpMethod.GET, "/api/eventos").permitAll()
 						.requestMatchers(HttpMethod.GET, "/api/eventos/{eventoId}").permitAll()
 						.requestMatchers(HttpMethod.GET, "/api/eventos/{eventoId}/imagen").permitAll()
+						.requestMatchers(HttpMethod.HEAD, "/api/eventos/{eventoId}/imagen").permitAll()
 						.requestMatchers(HttpMethod.GET, "/api/eventos/**")
 						.hasAnyAuthority(Roles.ROLE_ADMIN.toString(), Roles.ROLE_MANAGER.toString())
 						.requestMatchers(HttpMethod.POST, "/api/eventos/**")
