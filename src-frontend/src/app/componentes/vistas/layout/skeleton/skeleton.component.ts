ec6f4ba7a0d1484609c9810eee82b9249a6bf863
carsan-dev <crolyx16@gmail.com>
2026-01-23 14:11:29 +0100

    Ajustado el skeleton component para añadir el fab navigation de forma global en la app

--- src-frontend/src/app/componentes/vistas/layout/skeleton/skeleton.component.ts
+++ src-frontend/src/app/componentes/vistas/layout/skeleton/skeleton.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { BotonscrollComponent } from '../../../generales/botonscroll/botonscroll.component';
+import { FabNavegacionComponent } from '../../../generales/fab-navegacion/fab-navegacion.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,6 +12,7 @@ import { CommonModule } from '@angular/common';
     HeaderComponent,
     FooterComponent,
     BotonscrollComponent,
+    FabNavegacionComponent,
     CommonModule,
   ],
   templateUrl: './skeleton.component.html',
