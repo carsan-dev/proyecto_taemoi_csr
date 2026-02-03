30a8647bc2b212381190c05a8c6e9fe047cf73f1
carsan-dev <crolyx16@gmail.com>
2026-02-03 14:12:27 +0100

    Añadidos enlaces internos a los distintos componentes legales

--- src-frontend/src/app/componentes/vistas/legal/politica-cookies/politica-cookies.component.ts
+++ src-frontend/src/app/componentes/vistas/legal/politica-cookies/politica-cookies.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-politica-cookies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './politica-cookies.component.html',
   styleUrl: '../legal.shared.scss',
 })
